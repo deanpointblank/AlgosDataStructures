b038baf85d8a17b992b0ccb27eeec4dc682c7371
dean <deanlloyd64@gmail.com>
2020-08-16 12:33:24 -0400

    O of n

---
+++ Oofn.js
@@ -0,0 +1,13 @@
+const nemo = ['nemo']; // 2-7 milisecond runtime
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'] // 2-7 milisecond runtime
+const large = new Array(100000).fill('nemo') // 1223 - 1551 miliseconds runtime
+
+function findNemo(array){
+    for(let i = 0; i < array.length; i++){
+        if (array[i] === 'nemo'){
+            console.log('Found NEMO!');
+        }
+    }
+}
+
+findNemo(large)
