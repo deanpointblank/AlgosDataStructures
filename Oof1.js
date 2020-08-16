4714ad9283a5a240bcc6c2c31e34954a3d3cd643
dean <deanlloyd64@gmail.com>
2020-08-16 12:49:39 -0400

    O of 1

---
+++ Oof1.js
@@ -0,0 +1,8 @@
+const boxes = [0, 1, 2, 3, 4, 5]
+
+function logFirstTwoBoxes(boxes){
+    console.log(boxes[0]);
+    console.log(boxes[1]);
+}
+
+logFirstTwoBoxes(boxes) // O(1) -> constant time, regardless of elemets we will perform the same operatoins
