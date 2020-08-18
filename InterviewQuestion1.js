8c5c231b056b10f7c1d07b87d31bc4e96e500ea7
dean <deanlloyd64@gmail.com>
2020-08-18 12:14:30 -0400

    hash table solution given

--- InterviewQuestion1.js
+++ InterviewQuestion1.js
@@ -8,21 +8,59 @@
 //const array = ['z', 'y', 'x'];
 //should return true
 
-function commonItemsInArrays(array1, array2){
-    for(let i = 0; i < array1.length; i++){
-        for(let j = 0; j < array2.length; j++){
-            if(array1[i] === array2[j]){
-                console.log(true)
-                return true
-                break;
-            }
+// function commonItemsInArrays(array1, array2){
+//     for(let i = 0; i < array1.length; i++){
+//         for(let j = 0; j < array2.length; j++){
+//             if(array1[i] === array2[j]){
+//                 console.log(true)
+//                 return true
+//                 break;
+//             }
+//         }
+//     }
+//     console.log(false)
+//     return false
+// }
+
+// commonItemsInArrays(['a', 'b', 'c', 'x'], ['z', 'y', 'i'])
+// commonItemsInArrays(['a', 'b', 'c', 'x'], ['z', 'y', 'x'])
+
+//=> current solution is O(a * b)
+
+const array1 = ['a', 'b', 'c', 'x']
+const array2 = ['z', 'y', 'a']
+
+//array ==> obj {
+// a: true,
+// b: true,
+// c: true,
+// x: true
+//}
+// array[index] == obj.properties
+
+function containsCommonItem2(arr1, arr2){
+    //loop through first array and create object where properties == items in the array
+    let map = [];
+    for (let i = 0; i < arr1.length; i++){
+        if(!map[array1[i]]){
+            const item = array1[i];
+            map[item] = true;
+        }
+    }
+    console.log(map)
+    // loop through second array and check if item in 2nd array exists on created object
+    for (let j=0; j < arr2.length; j++){
+        if (map[array2[j]]){
+            console.log(true)
+            return true;
         }
     }
     console.log(false)
+
     return false
+
 }
 
-commonItemsInArrays(['a', 'b', 'c', 'x'], ['z', 'y', 'i'])
-commonItemsInArrays(['a', 'b', 'c', 'x'], ['z', 'y', 'x'])
+//o(a + b) Time Complexity 
 
-//=> current solution is O(a * b)
+containsCommonItem2(array1, array2)
