415d3472e427ed5787fc8a58851b6f1dcd240aec
dean <deanlloyd64@gmail.com>
2020-09-07 13:49:24 -0400

    added printList method to linkedlist

--- FirstLinkedList.js
+++ FirstLinkedList.js
@@ -13,6 +13,12 @@
 //         }
 //     }
 // }
+class Node {
+    constructor(value){
+        this.value = value;
+        next = null;
+    }
+}
 
 class LinkedList{
     constructor(value){
@@ -44,9 +50,24 @@ class LinkedList{
         this.length++
         
     }
+
+    printList(){
+        const array = []
+        let currentNode = this.head
+        while (!!currentNode){
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+
+    insert(index, value){
+
+    }
 }
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList)
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.printList())
