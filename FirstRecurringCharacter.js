24bb7af54b830595ebd5e11c3052a750c8a57e08
dean <deanlloyd64@gmail.com>
2020-09-07 11:35:03 -0400

    solved FirstRecurringCharacter

--- FirstRecurringCharacter.js
+++ FirstRecurringCharacter.js
@@ -8,3 +8,43 @@
 // Given an array = [2, 3, 4, 5]
 // It should return undefined
 
+function firstRecurringCharacter(array){
+    let characterHash = {}
+    let currentCharacterValue = []
+    let characterIndexDifference 
+
+    for (let i = 0; i < array.length; i++){
+        if(!characterHash[array[i]]){
+            characterHash[array[i]] = []
+        }
+        characterHash[array[i]].push(i)
+    }
+
+    for (let i = 0; i < array.length; i++){
+        if(characterHash[array[i]].length > 1){
+            // how can I tell which one is going to recur first?
+            // subtract first two element indexes in the array
+            characterIndexDifference = characterHash[array[i]][1] - characterHash[array[i]][0]
+            
+            if (currentCharacterValue.length === 0){
+                currentCharacterValue[0] = characterIndexDifference
+                currentCharacterValue[1] = array[i]
+            }
+            if( characterIndexDifference < currentCharacterValue[0] ){
+                currentCharacterValue[0] = characterIndexDifference
+                currentCharacterValue[1] = array[i]
+            }
+            
+        }
+        // console.log(characterHash)
+        
+    }
+
+    if(!!currentCharacterValue){
+        return currentCharacterValue[1]
+    } else {
+        return undefined
+    }
+}
+
+console.log(firstRecurringCharacter([2,5,5,2,3,5,1,2,4]))
